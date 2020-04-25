b615f3eb2b254645afa9ff1c1bf333f38d227c19
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-04-25 18:28:48 +0300

    Скачал заготовку проэка через start.spring.io

---
+++ src/main/java/ru/vienoulis/boot_crud/BootCrudApplication.java
@@ -0,0 +1,13 @@
+package ru.vienoulis.boot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCrudApplication.class, args);
+	}
+
+}
